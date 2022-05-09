52da3ac5023e8473403414bc5e1d5f84154a7b36
Abhishek Pai <abpai94@gmail.com>
2022-05-09 17:20:30 +0200

    Created MainPage and Resume to start testing components.

---
+++ src/components/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="testing the main page">
+        <h1> {{ msg }} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+</style>
